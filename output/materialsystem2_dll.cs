df08aad4d76c6acebe646f9be84e21b8f0500c9c
AmenHermano144 <lelgamerz@gmail.com>
2026-08-25 18:57:06 -0300

    825

--- output/materialsystem2_dll.cs
+++ output/materialsystem2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-23 22:43:40.221686 UTC
+// 2026-08-25 21:56:47.899785600 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: materialsystem2.dll
